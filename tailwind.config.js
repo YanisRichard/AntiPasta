7d667b94a75a63af4d301d45288f89130b0fb702
Hugo Paillard <116795550+HugoPaillard@users.noreply.github.com>
2023-05-16 08:49:08 +0200

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./C:\Hugo\MMI1\projet co\AntiPasta.{html,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
