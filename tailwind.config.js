3244e83864261c5ab1f1407d8602488d24feedc3
Hugo Paillard <hugo.paillard02@edu.univ-fcomte.fr>
2023-05-16 08:26:07 +0200

    changement config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./C:\Hugo\MMI1\projet co\AntiPasta.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
