b93f2fb778f1e77eb8222106e2cf550ba9ecfb1c
Guillaume Le Trequesser <113016302+gletrequess@users.noreply.github.com>
2023-05-16 09:22:23 +0200

    Routes & plugin pages

--- projet-antipasta/src/App.vue
+++ projet-antipasta/src/App.vue
@@ -1,20 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+	import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper bg-indigo-800 p-6">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+			<RouterLink to="/events">Agenda</RouterLink>
+      <RouterLink to="/contact">Contact</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
