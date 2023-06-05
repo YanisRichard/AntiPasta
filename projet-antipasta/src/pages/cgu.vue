925f98b74e057d65ca054d2f61b67f783bd8daa2
Guillaume Le Trequesser <113016302+gletrequess@users.noreply.github.com>
2023-06-05 14:28:03 +0200

    cgu fini

--- projet-antipasta/src/pages/cgu.vue
+++ projet-antipasta/src/pages/cgu.vue
@@ -3,7 +3,45 @@
 </script>
 
 <template>
-    <div class="mx-20  my-">
-        <h1>Conditions générales d'utilisation</h1>
+    <div class="mx-20  my-10">
+        <h1 class="my-5">Conditions générales d'utilisation</h1>
+
+        <p>Bienvenue sur notre site Anti-Pasta. En utilisant notre site, vous acceptez les présentes conditions générales d'utilisation. Si vous n'êtes pas d'accord avec ces conditions, veuillez ne pas utiliser le site.</p>
+
+        <p class="mt-5 mb-2">1. Contenu</p>
+        <p>Notre site propose des recettes de cuisine, des conseils culinaires et autres informations connexes. Nous nous efforçons de fournir des informations précises et à jour, mais nous ne garantissons pas l'exactitude, l'exhaustivité ou la pertinence de tout contenu sur notre site.</p>
+
+        <p class="mt-5 mb-2">2. Propriété intellectuelle</p>
+        <p>Tous les contenus de notre site, y compris les recettes, les images et autres éléments, sont protégés par les lois sur la propriété intellectuelle en vigueur. Vous acceptez de ne pas reproduire, distribuer, afficher publiquement, modifier, ou utiliser de quelque manière que ce soit le contenu de notre site sans notre consentement écrit préalable.</p>
+
+        <p class="mt-5 mb-2">3. Utilisation du site</p>
+        <p>Vous acceptez d'utiliser notre site conformément aux lois et réglementations en vigueur. Vous acceptez également de ne pas utiliser notre site pour : 
+            <li class="font-arial">
+            Publier des contenus illégaux, diffamatoires, offensants, menaçants, harcelants, haineux, discriminatoires ou pornographiques ;
+            </li>
+            <li class="font-arial">
+                Harceler ou intimider d'autres utilisateurs ;
+            </li>
+            <li class="font-arial">
+                Effectuer des actions qui pourraient endommager, désactiver, surcharger ou nuire au bon fonctionnement de notre site ou de tout autre équipement informatique ;
+            </li>
+            <li class="font-arial">
+                Utiliser notre site pour collecter des informations personnelles sur d'autres utilisateurs sans leur consentement.
+            </li>
+        </p>
+
+        <p class="mt-5 mb-2">5. Exclusion de responsabilité</p>
+        <p>Nous ne sommes pas responsables des dommages directs, indirects, accessoires, spéciaux ou consécutifs résultant de votre utilisation de notre site ou de l'impossibilité d'utiliser notre site. Nous ne garantissons pas que notre site sera exempt d'erreurs, de virus ou d'autres éléments nuisibles.</p>
+        
+        <p class="mt-5 mb-2">6. Modifications des conditions générales d'utilisation</p>
+        <p>Nous nous réservons le droit de modifier les présentes conditions générales d'utilisation à tout moment. Les modifications seront effectives dès leur publication sur notre site. Votre utilisation continue de notre site après la publication de ces modifications signifie que vous acceptez les nouvelles conditions.</p>
+
+        <p class="mt-5 mb-2">7. Loi applicable</p>
+        <p>Les présentes conditions générales d'utilisation sont régies par les lois en vigueur. Tout litige relatif à l'utilisation de notre site sera soumis à la juridiction compétente.
+        </p>
+
+        <p class="mt-5 mb-2">
+            Nous vous remercions d'avoir lu ces conditions générales d'utilisation et de respecter les règles énoncées ci-dessus. Si vous avez des questions ou des préoccupations, n'hésitez pas à nous contacter.
+        </p>
     </div>
 </template>
