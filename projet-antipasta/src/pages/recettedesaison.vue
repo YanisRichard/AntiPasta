82afb6fd050a3761c8b326658cc708860c8ac0bd
Guillaume Le Trequesser <guillaume.letrequesser@gmail.com>
2023-06-06 00:21:48 +0200

    Recettes V2

--- projet-antipasta/src/pages/recettedesaison.vue
+++ projet-antipasta/src/pages/recettedesaison.vue
@@ -6,22 +6,26 @@
 <div class="grid lg:grid-cols-2 lg:gap-4 mb-20">
 
     <div class="flex justify-center mb-20">
+
+        <a href="/recettes/recettesete">
         <div class="max-w-sm bg-white border rounded-lg transition-transform duration-300 transform-gpu hover:scale-105">
             <img class="rounded-t-lg" src="../../public/ete.webp" alt="Fond été" />
             <div class="p-5">
-                <a href="#">
+                <a href="/recettes/recettesete">
                 <h1 class="mb-2 font-bold tracking-tight text-gray-900 dark:text-white">Été</h1>
                 </a>
                 <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Rafraîchissez-vous avec nos délicieuses recettes estivales, parfaites pour les journées ensoleillées.</p>
-                <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <a href="/recettes/recettesete" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Découvrir plus
                     <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 </a>
             </div>
         </div>
+    </a>
     </div>
 
     <div class="flex justify-center mb-20">
+        <a href="/recettes/recettes_printemps">
         <div class="max-w-sm bg-white border rounded-lg transition-transform duration-300 transform-gpu hover:scale-105">
             <img class="rounded-t-lg" src="../../public/printemps.webp" alt="Fond printanier" />
             <div class="p-5">
@@ -35,9 +39,11 @@
                 </a>
             </div>
         </div>
+        </a>
     </div>
 
     <div class="flex justify-center mb-20">
+        <a href="/recettes/recettes_automne">
         <div class="max-w-sm bg-white border rounded-lg transition-transform duration-300 transform-gpu hover:scale-105">
             <img class="rounded-t-lg" src="../../public/automne.webp" alt="Fond automnal" />
             <div class="p-5">
@@ -51,6 +57,7 @@
                 </a>
             </div>
         </div>
+        </a>
     </div>
 
     <div class="flex justify-center mb-20">
