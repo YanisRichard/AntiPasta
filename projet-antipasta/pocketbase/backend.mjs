d973cc33e497e0ec52759edeccf2a339c063b1f5
Guillaume Le Trequesser <guillaume.letrequesser@gmail.com>
2023-06-06 00:49:21 +0200

    npm init -ys

--- projet-antipasta/pocketbase/backend.mjs
+++ projet-antipasta/pocketbase/backend.mjs
@@ -5,3 +5,9 @@ export async function ListeProfil(){
     const ProfilRecords = await pb.collection('profil').getFullList();
    return ProfilRecords;
 }
+
+export async function recettesbysaison(saison){
+    const trirecettesbyraison = await pb.collection('recettes').getFullList();
+    const recettesbysaison = recettes.filter(recette => recette.saison == saison);
+    return recettesbysaison;
+}
