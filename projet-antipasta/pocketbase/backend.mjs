edf73f494dd9166b5bd0f446520407877904361a
YanisRichard <yanis.richard17@gmail.com>
2023-06-06 00:49:50 +0200

    backend et testback test

--- projet-antipasta/pocketbase/backend.mjs
+++ projet-antipasta/pocketbase/backend.mjs
@@ -4,4 +4,5 @@ const pb = new PocketBase('http://127.0.0.1:8090');
 export async function ListeProfil(){
     const ProfilRecords = await pb.collection('profil').getFullList();
    return ProfilRecords;
-}
+};
+
--- projet-antipasta/pocketbase/testBack.js
+++ projet-antipasta/pocketbase/testBack.js
@@ -3,8 +3,8 @@ import { pb } from "./backend.mjs"
 import { ListeProfil } from "./backend.mjs";
 
 try {
-    const records = await ListeProfil();
-    console.log(JSON.stringify(records, null, 2));
+    const ProfilRecords = await ListeProfil();
+    console.log(JSON.stringify(ProfilRecords, null, 2));
 } catch (e) {
     console.error(e);
 }
