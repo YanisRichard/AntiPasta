7cb6ca916af66fa7980c01f0cf882f48fd0c4873
YanisRichard <yanis.richard17@gmail.com>
2023-06-06 03:25:33 +0200

    fiche ID faites

---
+++ projet-antipasta/src/pages/[Profil].vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {pb, Oneprofil } from '@/backend';
+import type { ProfilResponse } from '@/pocketbase-types';
+import type { ProfilRecord } from '@/pocketbase-types';
+import type { RouterLinkProps } from 'vue-router';
+import type { RouterViewProps } from 'vue-router';
+
+const id = defineProps<{ Profil : string }>();
+const profil = await Oneprofil(id.Profil);
+const img0 = profil.PhotoProfil
+const urlImg = pb.getFileUrl(profil, img0, {thumb: '306x306'});
+</script>
+
+<template>
+    <main class="ml-6 mr-6 mt-4 ">
+        <div>
+            <h2>{{ profil.Prenom }} {{ profil.Nom }}</h2>
+            <div class="flex justify-center">
+                <img :src="urlImg" alt="PhotoProfil">
+            </div>
+            <div>
+                {{ profil.Mail }}
+            </div>
+            <div>{{ profil.RecetteProposee }}
+            </div>
+        </div>
+
+    </main>
+</template>
---
+++ projet-antipasta/src/pages/[Recette].vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import {pb, Onerecette } from '@/backend';
+import type { RecetteResponse } from '@/pocketbase-types';
+import type { RecetteRecord } from '@/pocketbase-types';
+import type { RouterLinkProps } from 'vue-router';
+import type { RouterViewProps } from 'vue-router';
+
+const id = defineProps<{ Recette : string }>();
+const recette = await Onerecette(id.Recette);
+const img0 = recette.PhotoRecette
+const urlImg = pb.getFileUrl(recette, img0, {thumb: '306x218'});
+</script>
+
+<template>
+    <main class="ml-6 mr-6 mt-4 ">
+        <div>
+            <h2>{{ recette.NomRecette }}</h2>
+            <div class="flex justify-center">
+                <img :src="urlImg" alt="PhotoRecette">
+            </div>
+            <div>
+                {{ recette.Duree }}
+            </div>
+            <div>{{ recette.Nb_Personnes }}</div>
+            <div>
+                {{ recette.Ingredients }}
+            </div>
+            <div>
+                {{ recette.Recette }}
+            </div>
+            <div>
+
+            </div>
+        </div>
+
+    </main>
+</template>
