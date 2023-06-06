cb029fc399f4eea53f562856be058cfeba37cf44
YanisRichard <yanis.richard17@gmail.com>
2023-06-06 03:38:22 +0200

    backend dans le src

---
+++ projet-antipasta/src/backend.ts
@@ -0,0 +1,38 @@
+import PocketBase from 'pocketbase' ;
+import type { ProfilResponse, RecetteResponse } from '@/pocketbase-types';
+export const pb = new PocketBase('http://127.0.0.1:8090');
+
+export async function ListeProfil(){
+    const ProfilRecords = await pb.collection('profil').getFullList();
+   return ProfilRecords;
+
+};
+
+export async function ListeRecette(){
+    const RecetteRecords = await pb.collection('recette').getFullList();
+    return RecetteRecords;
+}
+
+export async function Onerecette(id: string){
+    const Onerecetterecord = await pb.collection('recette').getFullList();
+    const OnerecetterecordID = Onerecetterecord.filter(Onerecetterecord => Onerecetterecord.id === id);
+    return OnerecetterecordID;
+}
+
+export async function Oneprofil(id: string){
+    const Oneprofilrecord = await pb.collection('profil').getFullList();
+    const OneprofilrecordID = Oneprofilrecord.filter(Oneprofilrecord => Oneprofilrecord.id === id);
+    return OneprofilrecordID;
+}
+
+export async function nomrecetteparsaison(Saison: string){
+    const RecetteRecords = await pb.collection('recette').getFullList();
+    const RecetteRecordsSaison = RecetteRecords.filter(RecetteRecords => RecetteRecords.Saisons === Saison);
+    return RecetteRecordsSaison;
+}
+
+export async function Recettecreeparprofil(id: string){
+    const Recettecreeparprofillist = await pb.collection('recette').getFullList();
+    const RecetteRecordsProfil = Recettecreeparprofillist.filter(Recettecreeparprofillist => Recettecreeparprofillist.CreePar === id);
+    return RecetteRecordsProfil;
+}
