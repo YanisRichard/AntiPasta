ceb03966b09a3cf85e55f043117fc00e848948f3
Guillaume Le Trequesser <guillaume.letrequesser@gmail.com>
2023-06-06 03:15:16 +0200

    Toutes les fonctions saloperie

--- projet-antipasta/pocketbase/backend.mjs
+++ projet-antipasta/pocketbase/backend.mjs
@@ -12,10 +12,26 @@ export async function ListeRecette(){
     return RecetteRecords;
 }
 
-// export async function Recettesparsaisons(){
-//     const RecettesRecords = await pb.collection('recette').getFullList({
-//         filter: recette.Saisons = '${Saisons}' ,
-//         expand: 'recette',
-//     });
-//     return RecettesRecords;
-// }
+export async function Onerecette(id){
+    const Onerecetterecord = await pb.collection('recette').getFullList();
+    const OnerecetterecordID = Onerecetterecord.filter(Onerecetterecord => Onerecetterecord.id === id);
+    return OnerecetterecordID;
+}
+
+export async function Oneprofil(id){
+    const Oneprofilrecord = await pb.collection('profil').getFullList();
+    const OneprofilrecordID = Oneprofilrecord.filter(Oneprofilrecord => Oneprofilrecord.id === id);
+    return OneprofilrecordID;
+}
+
+export async function nomrecetteparsaison(Saison){
+    const RecetteRecords = await pb.collection('recette').getFullList();
+    const RecetteRecordsSaison = RecetteRecords.filter(RecetteRecords => RecetteRecords.Saisons === Saison);
+    return RecetteRecordsSaison;
+}
+
+export async function Recettecreeparprofil(id){
+    const Recettecreeparprofillist = await pb.collection('recette').getFullList();
+    const RecetteRecordsProfil = Recettecreeparprofillist.filter(Recettecreeparprofillist => Recettecreeparprofillist.CreePar === id);
+    return RecetteRecordsProfil;
+}
--- projet-antipasta/pocketbase/testBack.js
+++ projet-antipasta/pocketbase/testBack.js
@@ -1,5 +1,10 @@
 // import { ListeProfil } from "./backend.mjs";
-import { ListeRecette } from "./backend.mjs";
+// import { ListeRecette } from "./backend.mjs";
+// import { nomrecetteparsaison } from "./backend.mjs";
+// import { Recettecreeparprofil } from "./backend.mjs";
+import { Onerecette } from "./backend.mjs";
+import { Oneprofil } from "./backend.mjs";
+
 
 // // try {
 // //     const records = await ListeProfil();
@@ -15,9 +20,37 @@ import { ListeRecette } from "./backend.mjs";
 //     console.error(e);
 // }
 
-try{
-    const RecetteRecords = await ListeRecette();
-    console.log(JSON.stringify(RecetteRecords, null, 2));
-} catch (e) {
-    console.error(e);
-}
+// try{
+//     const RecetteRecords = await ListeRecette();
+//     console.log(JSON.stringify(RecetteRecords, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
+
+// try{
+//     const RecetteRecordsSaison = await nomrecetteparsaison("Hiver");
+//     console.log(JSON.stringify(RecetteRecordsSaison, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
+
+// try{
+//     const RecetteRecordsProfil = await Recettecreeparprofil("wymleqxj87ntbr9");
+//     console.log(JSON.stringify(RecetteRecordsProfil, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
+
+// try{
+//     const OnerecetterecordID = await Onerecette("re6ekznebq5w1ps");
+//     console.log(JSON.stringify(OnerecetterecordID, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
+
+// try{
+//     const OneprofilrecordID = await Oneprofil("wymleqxj87ntbr9");
+//     console.log(JSON.stringify(OneprofilrecordID, null, 2));
+// } catch (e) {
+//     console.error(e);
+// }
